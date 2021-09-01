ef4efda883f1d2d1a4be39d1f305bf34515557ee
chorng <chung.horng@sinergise.com>
2021-09-01 09:35:03 +0200

    rename bands asset and clean up the code

--- src/stactools/sentinel3/stac.py
+++ src/stactools/sentinel3/stac.py
@@ -3,30 +3,18 @@ import os
 
 import pystac
 from pystac.extensions.eo import EOExtension
-from pystac.extensions.sat import SatExtension
 from pystac.extensions.projection import ProjectionExtension
+from pystac.extensions.sat import SatExtension
 
+from .constants import (SENTINEL_CONSTELLATION, SENTINEL_LICENSE,
+                        SENTINEL_OLCI_BANDS, SENTINEL_PROVIDER,
+                        SENTINEL_SLSTR_BANDS)
 from .metadata_links import MetadataLinks
 from .product_metadata import ProductMetadata
-
-from .constants import (
-    SENTINEL_PROVIDER,
-    SENTINEL_CONSTELLATION,
-    SENTINEL_LICENSE,
-    SENTINEL_SLSTR_BANDS,
-    SENTINEL_OLCI_BANDS
-)
-
-from .properties import (
-    fill_sat_properties,
-    fill_proj_properties,
-    fill_eo_properties
-)
-
-# from .bands import image_asset_from_href
+from .properties import (fill_eo_properties, fill_proj_properties,
+                         fill_sat_properties)
 
 logger = logging.getLogger(__name__)
-
 
 def create_item(granule_href: str) -> pystac.Item:
     """Create a STC Item from a Sentinel-3 scene.
@@ -51,33 +39,33 @@ def create_item(granule_href: str) -> pystac.Item:
         properties={},
         stac_extensions=[],
     )
-    
+
     # ---- Add Extensions ----
     # sat
     sat = SatExtension.ext(item, add_if_missing=True)
     fill_sat_properties(sat, metalinks.product_metadata_href)
-    
+
     # eo
     eo = EOExtension.ext(item, add_if_missing=True)
     fill_eo_properties(eo, metalinks.product_metadata_href)
-    
+
     # proj
     proj = ProjectionExtension.ext(item, add_if_missing=True)
     fill_proj_properties(proj, product_metadata)
-    
+
     # --Common metadata--
     item.common_metadata.providers = [SENTINEL_PROVIDER]
     item.common_metadata.platform = product_metadata.platform
     item.common_metadata.constellation = SENTINEL_CONSTELLATION
-    
+
     # s3 properties
     item.properties.update({**product_metadata.metadata_dict})
-    
+
     # Add assets to item
     item.add_asset(*metalinks.create_manifest_asset())
-    
+
     # objects for bands
-    
+
     item.add_asset(*metalinks.create_band_asset())
-    
+
     return item
